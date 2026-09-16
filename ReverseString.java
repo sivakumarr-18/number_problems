edb2e9d877c536a65888b9f3dca5a3f67fcdae0f
SIVAKUMAR A <sivamumar12459@gmail.com>
2026-09-16 22:25:26 +0530

    Create ReverseString.java

---
+++ ReverseString.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+class ReverseString {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String str = sc.nextLine();
+
+        String reverse = "";
+
+        for (int i = str.length() - 1; i >= 0; i--) {
+            reverse = reverse + str.charAt(i);
+        }
+
+        System.out.println("Reverse = " + reverse);
+    }
+}
