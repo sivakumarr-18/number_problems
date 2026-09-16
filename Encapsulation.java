1ea514b6f73c99cc3171d3bc1cb44b0ce0995d31
SIVAKUMAR A <sivamumar12459@gmail.com>
2026-09-16 22:26:44 +0530

    Create Encapsulation.java

---
+++ Encapsulation.java
@@ -0,0 +1,32 @@
+class Student {
+    private String name;
+    private int age;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
+
+class Encapsulation {
+    public static void main(String[] args) {
+        Student s = new Student();
+
+        s.setName("Siva");
+        s.setAge(20);
+
+        System.out.println("Name = " + s.getName());
+        System.out.println("Age = " + s.getAge());
+    }
+}
